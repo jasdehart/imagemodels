cde3ad22ab5daa476d088b77f358079144b24c66
Lisa Egede <lisaegede@ou.edu>
2019-04-05 14:31:19 -0500

    update to fix error loading images, updated images

--- TransferLearning/runmodel.py
+++ TransferLearning/runmodel.py
@@ -9,6 +9,7 @@ import copy
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import os.path
 import sys
 import time
 import torch
@@ -20,6 +21,10 @@ from torch.optim import lr_scheduler
 from torchvision.transforms import ToTensor
 from torchvision import datasets, models, transforms
 from PIL import Image
+
+from pprint import pprint
+
+
 
 '''
 def main() calls getfiles() function and passes in directory from command line which
@@ -36,37 +41,40 @@ def main(directory, model):
     running_model_predictions(paths, model)
 
 def getfiles(directory):
-    fileList = []
+    '''fileList = []'''
+    fullPathList = []
     
     for filename in os.listdir(directory):
         fnc = directory + "/" + filename
         print("PRINTING" + fnc)
         if os.path.isdir(fnc):
-            '''print("is in directory path for " + fnc)'''
+            print("is in directory path for " + fnc)
             for filename in os.listdir(fnc):
                 fileDir = fnc + "/" + filename
+                print("is inside in directory path for " + fileDir)
+                print(filename)
                 if os.path.isdir(fileDir):
                     for filename in os.listdir(fileDir):
                         if filename.endswith(".jpg"):
-                            print("printing filename ending with .jpg" + filename)
+                            print("***" + fileDir + "/" + filename)
                             fileList.append(str(filename))
                         if filename.endswith(".gif"):
-                            print("printing filename ending with .gif" + filename)
-                            fileList.append(str(filename))
+                            sys.stderr.write("ERROR: Classifier only takes .jpg files\n")
+                        #fileList.append(str(filename))
                         if filename.endswith(".png"):
                             sys.stderr.write("ERROR: Classifier only takes .jpg and .gif files\n")
                         if filename.endswith(".jpeg"):
                             sys.stderr.write("ERROR: Classifier only takes .jpg and .gif files\n")
-    for filename in list(fileList):
-        fn = directory + '/' + filename
-        fullPathList.append(str(fn))
+                    for filename in list(fileList):
+                        fn = fileDir + '/' + filename
+                        '''fn = directory + '/' + filename'''
+                        fullPathList.append(str(fn))
+        pprint(fullPathList)
     return fullPathList
-
 
 #prediction function
 def predict_image(image, model):
     print("hello predict image")
-
         #boundaries for transforming the image
     test_transforms = transforms.Compose([
             transforms.RandomResizedCrop(224),
@@ -83,9 +91,11 @@ def predict_image(image, model):
 
 #running predictions with files
 def running_model_predictions(path, model):
+    print(type(path))
     print("inside running model")
     for filename in list(path):
-        X = Image.open(filename)
+        if(os.path.isfile(filename)):
+            X = Image.open(filename)
         print(X)
         print('***********************************************************')
         print("Current file: " + filename)
@@ -102,6 +112,7 @@ def running_model_predictions(path, model):
             index = 'prediction: license'
         if (index == 4):
             index = 'prediction: passport'
+        
 
         print (index)
 
